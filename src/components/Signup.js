7aa1addff5cd1687ca185db14bdd4bc6c1a9f5c9
imonoho <imonoho@gmail.com>
2021-01-28 21:14:42 -0600

    Start

---
+++ src/components/Signup.js
@@ -0,0 +1,42 @@
+import {Card, Form, Button} from 'react-bootstrap';
+import {useRef} from "react";
+
+
+function Signup() {
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+
+  return (
+    <>
+      <Card>
+        <Card.Body >
+          <h2 className='text-center mb-4'>Sign Up</h2>
+          <Form>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type='email' reqired ref={emailRef}/>
+            </Form.Group>
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' reqired ref={passwordRef}/>
+            </Form.Group>
+            <Form.Group id='password-confirm'>
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type='password' reqired ref={passwordConfirmRef}/>
+            </Form.Group>
+            <Button className='w-100' type='submit'>
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        Already have an account? Log In
+      </div>
+    </>
+  );
+}
+
+export default Signup;
